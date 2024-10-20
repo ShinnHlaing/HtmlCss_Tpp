33ec110fdf683947273179e9097ea93ad62fd248
ShinnHlaing <hlaingthazinphoo.hp@gmail.com>
2024-10-20 09:01:31 +0630

    day6 assginment

---
+++ Day6/day6.js
@@ -0,0 +1,8 @@
+function show() {
+  let menu = document.getElementById("hbMenu");
+  if (menu.style.display === "block") {
+    menu.style.display = "none";
+  } else {
+    menu.style.display = "block";
+  }
+}
