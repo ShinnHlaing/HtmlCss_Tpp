b753b195776fe6f2659f45f18555ed0e80e116cb
ShinnHlaing <hlaingthazinphoo.hp@gmail.com>
2024-10-22 11:23:49 +0630

    day8 responsive

---
+++ Day8/day8.js
@@ -0,0 +1,8 @@
+function show() {
+  let menu = document.getElementById("hbMenu");
+  if (menu.style.display === "none") {
+    menu.style.display = "flex";
+  } else {
+    menu.style.display = "none";
+  }
+}
